b6bec09007d19f4b1296869e2b15cbb03ebe1e27
HuyHG <tudutuduuuu@icloud.com>
2024-01-22 13:51:03 +0700

    Thông điệp commit của bạn

--- src/index.js
+++ src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import { ContextProvider } from './hooks/useStateContext';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+  typography:{
+    fontFamily: '"IBM Plex Sans"'
+  }
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </ContextProvider>
   </React.StrictMode>
 );
 
